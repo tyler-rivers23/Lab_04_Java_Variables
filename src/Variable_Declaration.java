18ef4866a8e6e4b33fe7b90c619908f18733dbb2
videogamelife8 <tyler.rivers2023@gmail.com>
2024-09-17 17:07:27 -0400

    Completed Part 1

---
+++ src/Variable_Declaration.java
@@ -0,0 +1,11 @@
+public class Variable_Declaration {
+
+
+    public static void main(String[] args) {
+
+        int number_of_kids = 1;
+        boolean is_it_Raining = false;
+        double price_of_gas = 2.89;
+        int fav_number = 5;
+    }
+}
