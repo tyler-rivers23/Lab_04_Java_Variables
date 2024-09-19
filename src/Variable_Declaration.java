1a11a4f05f54666d4ba4bff1d3c7687a1d138d4c
tyler-rivers23 <riverste@mail.uc.edu>
2024-09-19 12:40:05 -0400

    Update Variable_Declaration.java

--- src/Variable_Declaration.java
+++ src/Variable_Declaration.java
@@ -7,5 +7,8 @@ public class Variable_Declaration {
         boolean is_it_Raining = false;
         double price_of_gas = 2.89;
         int fav_number = 5;
+        double shoe_size = 10.0;
+        String birthMonth = "June";
+        String fullName = "Tyler Rivers";
     }
 }
